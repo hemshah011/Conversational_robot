17efc46f2f549abc95e88a10650e631c129d0d0c
hemshah011 <64735493+hemshah011@users.noreply.github.com>
2020-05-14 21:57:49 +0530

    Delete chatbotcmd.py

--- chatbotcmd.py
+++
@@ -1,8 +0,0 @@
-import aiml
-kernel = aiml.Kernel()
-kernel.learn("std-startup.xml")
-kernel.respond("load aiml b")
-print("Hey,I am Jarvis your virtual assistant")
-#crl+c to quit.
-while True:
-    print(kernel.respond(input("Here to help :")))
